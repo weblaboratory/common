1139614ef463da50a7b78c555a78cd41e8210067
Timkor <timothy.korse@gmail.com>
2015-11-17 14:50:13 +0100

    Basic string features

--- library/Weblab/Common/String.php
+++ library/Weblab/Common/String.php
@@ -15,4 +15,36 @@ class String {
         $this->data = $data;
     }
     
+    public function concatenate($value) {
+        return new String($this->data . String::data($value));
+    }
+    
+    public function indexOf($substring) {
+        $pos = strpos($this->data, String::data($substring));
+        return $pos !== false ? $pos : -1;
+    }
+    
+    public function substring($index, $length = null) {
+        return new String(substr($this->data, $index, $length));
+    }
+    
+    public static function fix($value) {
+        if ($value instanceof String) {
+            return $value;
+        } else if (is_string($value)) {
+            return new String($value);
+        } else {
+            throw new \RuntimeException("Can't coerce value into String");
+        }
+    }
+    
+    private static function data($value) {
+        if ($value instanceof String) {
+            return $value->data;
+        } else if (is_string($value)) {
+            return $value;
+        } else {
+            throw new \RuntimeException("Can't coerce value into plain string");
+        }
+    }
 }
